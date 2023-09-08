194f08b50e650173dfb807ee01dadad3362477d2
横茶横葉 <108619120+YokochaYokoha@users.noreply.github.com>
2023-09-08 20:07:37 +0900

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,12 +2,23 @@
   "name": "現在時刻 by 横茶横葉",
     "description": "とってもシンプルな時計です!",
     "display": "standalone",
+    "background_color": "#ffffff",
     "short_name": "現在時刻",
     "start_url": "/time",
     "scope": "/time",
-    "icons":{[
+    "icons":[
+    {
     "sizes": "512x512",
     "src": "/timeico.png"
-    "type": "image/*"
-             ]}
+    "type": "image/pmg"
+     "purpose": "any maskable"
+},
+{
+    "sizes": "192x192",
+    "src": "/timeicos.png"
+    "type": "image/pmg"
+     "purpose": "any maskable"
+  
+}
+            
 }
